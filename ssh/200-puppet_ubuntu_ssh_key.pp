0d245f87ec1a7aa820bea5be316a06708f390e21
Njoefreeman <j.nindorera@alustudent.com>
2024-03-16 10:12:13 -0700

    200-puppet_ubuntu_ssh_key.pp

---
+++ ssh/200-puppet_ubuntu_ssh_key.pp
@@ -0,0 +1,19 @@
+# File: 200-puppet_ubuntu_ssh_key.pp
+
+# Ensure the ~/.ssh directory exists for the ubuntu user
+file { '/home/ubuntu/.ssh':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0700',
+}
+
+# Add the provided SSH public key to the authorized_keys file for the ubuntu user
+file { '/home/ubuntu/.ssh/authorized_keys':
+  ensure => present,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0600',
+  content => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN\n",
+}
+
